40543f4dad1acab220e9756ef1b0d7d4060c6647
Raisa Toscano <daisa1690@gmail.com>
2025-07-21 05:06:39 -0600

    disconnectedCallback

---
+++ disconnectedCallback/app.js
@@ -0,0 +1,14 @@
+class MyCustomElement extends HTMLElement {
+    constructor() {
+        super()
+        console.log("Hola desde el constructor - Memoria");
+    }
+connectedCallback(){
+    console.log("Hola desde el DOM");
+}
+disconnectedCallback(){
+    console.log("Adios al DOM");
+}
+}
+customElements.define("my-custom-element", MyCustomElement);
+document.querySelector("my-custom-element").remove();
